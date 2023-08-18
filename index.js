4c92964329b81be8ae2a66130506db9402ab6814
autovagas <bruno.guimaraes@autovagas.com.br>
2023-08-18 11:50:49 -0300

    chore: add pg setup ad dot env setup

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const nome = 'Bruno';
+import 'dotenv/config.js';
+import express from 'express';
+import { PostgresHelper } from './src/database/postgres/helper.js';
 
-console(nome);
+const app = express();
+
+app.get('/', async (req, res) => {
+    const results = await PostgresHelper.query('SELECT * FROM users;');
+    return res.send(JSON.stringify(results));
+});
+
+app.listen(3030, () => console.log('listening on port 3030'));
