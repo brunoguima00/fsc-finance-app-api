cade606266226a0abdc06fdfe3fabed41d0317ba
autovagas <bruno.guimaraes@autovagas.com.br>
2023-08-22 21:21:10 -0300

    feat: add PostgresUserRepository

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 import 'dotenv/config.js';
 import express from 'express';
-import { PostgresHelper } from './src/database/postgres/helper.js';
 
 const app = express();
 
-app.get('/', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users;');
-    return res.send(JSON.stringify(results));
-});
+app.use(express.json());
 
 app.listen(3030, () => console.log('listening on port 3030'));
