c2464c4e44172eaf159a584f6e8d1e0821d72ed0
autovagas <bruno.guimaraes@autovagas.com.br>
2023-08-22 21:47:48 -0300

    feat: add createUserUseCase.js

---
+++ src/useCases/createUser/createUserUseCase.js
@@ -0,0 +1,26 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../../repositories/postgres/user/createUserRepository';
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // Gerar ID do usuário
+        const userId = uuidv4();
+        // Criptografar a senha do usuário
+        const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+        // Inserir Usuário no database
+        const user = {
+            ...createUserParams,
+            id: userId,
+            password: hashedPassword,
+        };
+
+        // Chamar o repositório
+        const postgresCreateUserRepository = new PostgresCreateUserRepository();
+
+        const createdUser = await postgresCreateUserRepository.execute(user);
+
+        return createdUser;
+    }
+}
