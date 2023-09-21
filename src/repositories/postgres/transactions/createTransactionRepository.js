27d7f1db2d4b1035411aeb3783e39dae5e5b8212
autovagas <bruno.guima00@gmail.com>
2023-09-21 12:25:45 -0300

    feat: add PostgresCreateTransactionRepository

---
+++ src/repositories/postgres/transactions/createTransactionRepository.js
@@ -0,0 +1,21 @@
+import { PostgresHelper } from '../../../database/postgres/helper';
+
+export class PostgresCreateTransactionRepositorory {
+    async execute(createTransactionParams) {
+        const createdTransaction = await PostgresHelper.query(
+            `INSERT INTO transactions(id, userid, name, date, amount, type) 
+            VALUES ($1, $2, $3, $4, $5, $6) 
+            RETURNING * 
+            `,
+            [
+                createTransactionParams.id,
+                createTransactionParams.userid,
+                createTransactionParams.name,
+                createTransactionParams.date,
+                createTransactionParams.amount,
+                createTransactionParams.type,
+            ],
+        );
+        return createdTransaction[0];
+    }
+}
