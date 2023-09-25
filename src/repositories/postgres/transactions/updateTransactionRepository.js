fe70a820d4a96c46111d3b39b8783c67629f77ef
autovagas <bruno.guima00@gmail.com>
2023-09-25 19:41:21 -0300

    feat: add PostgresUpdatetransactionRepository

---
+++ src/repositories/postgres/transactions/updateTransactionRepository.js
@@ -0,0 +1,25 @@
+import { PostgresHelper } from '../../../database/postgres/helper.js';
+
+export class PostgresUpdatetransactionRepository {
+    async execute(transactionId, updateTransactionParams) {
+        const updateFields = [];
+        const updateValues = [];
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`);
+            updateValues.push(updateTransactionParams[key]);
+        });
+        updateValues.push(transactionId);
+
+        const updateQuery = `UPDATE transactions SET ${updateFields.join(
+            ', ',
+        )} WHERE id = $${updateValues.length} RETURNING *`;
+
+        const updatedtransaction = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        );
+
+        return updatedtransaction[0];
+    }
+}
