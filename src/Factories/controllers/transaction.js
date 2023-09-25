9a7d8f41dff03f04d6e3129a856ae5dee05c310f
autovagas <bruno.guima00@gmail.com>
2023-09-25 19:52:39 -0300

    feat: add UpdateTransactionUseCase

--- src/Factories/controllers/transaction.js
+++ src/Factories/controllers/transaction.js
@@ -3,7 +3,7 @@ import { CreateTransactionUseCase } from '../../useCases/transactions/createTran
 import { CreateTransactionController } from '../../controllers/transactions/createTransaction/createTransactionController.js';
 import { PostgresGetUserByIdRepository } from '../../repositories/postgres/user/getUserByIdRepository.js';
 import { PostgresGetTransactionsByUserIdRepository } from '../../repositories/postgres/transactions/getTransactionsByUserIdRepository.js';
-import { GetTransactionsByUserIdUseCase } from '../../useCases/transactions/createTransaction/getTransactionsByUserIdUseCase.js';
+import { GetTransactionsByUserIdUseCase } from '../../useCases/transactions/getTransactionsByUserId/getTransactionsByUserIdUseCase.js';
 import { GetTransactionsByUserIdController } from '../../controllers/transactions/getTransactionsByUserIdController/getTransactionsByUserIdController.js';
 
 export const makeCreateTransactionController = () => {
