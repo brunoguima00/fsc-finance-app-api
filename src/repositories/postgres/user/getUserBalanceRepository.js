1d285dee141f47f9c0452ed0273f23e842506e02
autovagas <bruno.guima00@gmail.com>
2024-02-05 20:19:37 -0300

    feat: add PostgresGetUserBalanceRepository

---
+++ src/repositories/postgres/user/getUserBalanceRepository.js
@@ -0,0 +1,18 @@
+import { PostgresHelper } from '../../../database/postgres/helper.js';
+export class PostgresGetUserBalanceRepository {
+    async execute(userId) {
+        const balance = await PostgresHelper.query(
+            `SELECT
+            SUM(CASE WHEN type='EARNING' THEN amount ELSE 0 END) AS earnings,
+            SUM(CASE WHEN type='EXPENSE' THEN amount ELSE 0 END) AS expenses,
+            SUM(CASE WHEN type='INVESTMENT' THEN amount ELSE 0 END) AS investments,
+            (
+                SUM(CASE WHEN type='EARNING' THEN amount ELSE 0 END)
+                -  SUM(CASE WHEN type='EXPENSE' THEN amount ELSE 0 END)
+                - SUM(CASE WHEN type='INVESTMENT' THEN amount ELSE 0 END)
+            ) AS balance FROM transactions where userId = $1`,
+            [userId],
+        );
+        return balance[0];
+    }
+}
