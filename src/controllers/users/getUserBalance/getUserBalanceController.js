9e826f3c33108a07249910f76bf378fed21e2e41
autovagas <bruno.guima00@gmail.com>
2024-02-05 21:19:04 -0300

    feat: add getUserBalanceController

---
+++ src/controllers/users/getUserBalance/getUserBalanceController.js
@@ -0,0 +1,41 @@
+import { UserNotFoundError } from '../../../Errors/user.js';
+
+import {
+    serverError,
+    userNotFoundResponse,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+} from '../../helpers/index.js';
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase;
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId;
+
+            const idIsValid = checkIfIdIsValid(userId);
+
+            if (!idIsValid) {
+                return invalidIdResponse();
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({
+                userId,
+            });
+
+            return ok(balance);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return userNotFoundResponse();
+            }
+
+            console.log(error);
+
+            return serverError();
+        }
+    }
+}
