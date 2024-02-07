5a9251dbe652188299bac8e7471a51f24214f299
autovagas <bruno.guima00@gmail.com>
2024-02-07 20:50:17 -0300

    feat: add zod to create user controller

--- src/controllers/users/createUser/createUserController.js
+++ src/controllers/users/createUser/createUserController.js
@@ -1,15 +1,7 @@
 import { EmailAlreadyInUseError } from '../../../Errors/user.js';
-import {
-    invalidPasswordResponse,
-    emailIsAlreadyUseResponse,
-    checkIfPasswordIsValid,
-    checkIfEmailIsValid,
-    badRequest,
-    created,
-    serverError,
-    validateRequiredFields,
-    requiredFieldsIsMissingResponse,
-} from '../../helpers/index.js';
+import { createUserSchema } from '../../../schemas/user.js';
+import { badRequest, created, serverError } from '../../helpers/index.js';
+import { ZodError } from 'zod';
 
 export class CreateUserController {
     constructor(createUserUseCase) {
@@ -19,43 +11,19 @@ export class CreateUserController {
         try {
             const params = httpRequest.body;
 
-            // validar a requisição(campos obrigatórios, tamanho de senha, se o email é valido)
-            const requiredFields = ['firstName', 'email', 'password'];
-
-            const { ok: someRequiredFieldWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields);
-
-            if (!someRequiredFieldWereProvided) {
-                return requiredFieldsIsMissingResponse(missingField);
-            }
-            // Verificar pq esse for usa of ao invés de in
-            for (const field of requiredFields) {
-                if (!params[field] || params[field].trim().length == 0) {
-                    return badRequest({ message: `Missing param: ${field}` });
-                }
-            }
-
-            // Verify if password has more than 6 characters
-            const passwordIsValid = checkIfPasswordIsValid(params.password);
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponse();
-            }
-            // Validar o email
-            const emailIsValid = checkIfEmailIsValid(params.email);
-
-            if (!emailIsValid) {
-                return emailIsAlreadyUseResponse();
-            }
+            await createUserSchema.parseAsync(params);
 
             // chamar o use case
-
             const createdUser = await this.createUserUseCase.execute(params);
 
             // retornar a resposta para o usuário (status code or body)
-
             return created(createdUser);
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.errors[0].message,
+                });
+            }
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({ message: error.message });
             }
---
+++ src/schemas/user.js
@@ -0,0 +1,42 @@
+import { z } from 'zod';
+
+export const createUserSchema = z.object({
+    firstName: z
+        .string({
+            required_error: 'Firstname is required',
+        })
+        .trim()
+        .min(3, {
+            message: 'First name is required',
+        }),
+    lastName: z
+        // Quando o campo não é passado
+        .string({
+            required_error: 'Lastname is required',
+        })
+        .trim()
+        // Quando o campo não tem o mínimo de caracteres
+        .min(3, {
+            message: 'Lastname must be at leat 3 characters',
+        }),
+    email: z
+        // O required padrão é apenas se o campo não estiver presente, assim usamos
+        .string({
+            required_error: 'Email is required',
+        })
+        .email({
+            message: 'Please provide a valid email',
+        })
+        .trim()
+        .min(10, {
+            message: 'Email is required',
+        }),
+    password: z
+        .string({
+            required_error: 'Password ir required',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must leat at 6 characters',
+        }),
+});
