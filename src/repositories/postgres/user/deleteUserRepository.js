f4b199aebcb80d8d56404519a3654121027ee39c
autovagas <bruno.guima00@gmail.com>
2024-02-17 22:27:08 -0300

    feat: change to prisma

--- src/repositories/postgres/user/deleteUserRepository.js
+++ src/repositories/postgres/user/deleteUserRepository.js
@@ -1,11 +1,13 @@
-import { PostgresHelper } from '../../../database/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresDeleteUserRepository {
     async execute(userId) {
-        const user = await PostgresHelper.query(
-            'DELETE FROM users WHERE id=$1 RETURNING *',
-            [userId],
-        );
-        return user[0];
+        const user = await prisma.users.delete({
+            where: {
+                id: userId,
+            },
+        });
+
+        return user;
     }
 }
