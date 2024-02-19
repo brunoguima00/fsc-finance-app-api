9b15ccdaa4e70f5f664ab4d596d0450f0a59c07c
autovagas <bruno.guima00@gmail.com>
2024-02-18 21:32:12 -0300

    feat: change updateTransaction repository to prisma

--- src/repositories/postgres/transactions/updateTransactionRepository.js
+++ src/repositories/postgres/transactions/updateTransactionRepository.js
@@ -1,25 +1,14 @@
-import { PostgresHelper } from '../../../database/postgres/helper.js';
+import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresUpdateTransactionRepository {
     async execute(transactionId, updateTransactionParams) {
-        const updateFields = [];
-        const updateValues = [];
-
-        Object.keys(updateTransactionParams).forEach((key) => {
-            updateFields.push(`${key} = $${updateValues.length + 1}`);
-            updateValues.push(updateTransactionParams[key]);
+        const updatedTransaction = await prisma.transactions.update({
+            where: {
+                id: transactionId,
+            },
+            data: updateTransactionParams,
         });
-        updateValues.push(transactionId);
 
-        const updateQuery = `UPDATE transactions SET ${updateFields.join(
-            ', ',
-        )} WHERE id = $${updateValues.length} RETURNING *`;
-
-        const updatedtransaction = await PostgresHelper.query(
-            updateQuery,
-            updateValues,
-        );
-
-        return updatedtransaction[0];
+        return updatedTransaction;
     }
 }
