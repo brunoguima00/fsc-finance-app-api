59c31c37ad79ed4e798ae7e5bc5330a7532c096d
Bruno Guimaraes <bruno.guima00@gmail.com>
2025-06-03 18:17:53 -0300

    feat: ensure return 400 if email and password is not provides

--- src/controllers/users/createUser/createUser.test.js
+++ src/controllers/users/createUser/createUser.test.js
@@ -54,4 +54,69 @@ describe('Create User Controller', () => {
         // Assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('should return 400 if lastName is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+
+        const createUserController = new CreateUserController(
+            createUserUseCase
+        );
+
+        const httpRequest = {
+            body: {
+                firstName: 'Bruno',
+                email: '',
+                password: '1234456789',
+            },
+        };
+        // Act
+        const retult = await createUserController.execute(httpRequest);
+
+        // Assert
+        expect(retult.statusCode).toBe(400);
+    });
+
+    it('should return 400 if email is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+
+        const createUserController = new CreateUserController(
+            createUserUseCase
+        );
+
+        const httpRequest = {
+            body: {
+                firstName: 'Bruno',
+                lastName: 'Soares',
+                password: '1234456789',
+            },
+        };
+        // Act
+        const result = await createUserController.execute(httpRequest);
+        // Assert
+        expect(result.statusCode).toBe(400);
+    });
+
+    it('should return 400 if password is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+
+        const createUserController = new CreateUserController(
+            createUserUseCase
+        );
+
+        const httpRequest = {
+            body: {
+                firstName: 'Bruno',
+                lastName: 'Soares',
+                email: '',
+            },
+        };
+        // Act
+        const result = await createUserController.execute(httpRequest);
+
+        // Assert
+        expect(result.statusCode).toBe(400);
+    });
 });
