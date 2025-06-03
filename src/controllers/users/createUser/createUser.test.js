fa8965cacfc0415bb5c03143d05a2e4fcd7f5c1a
Bruno Guimaraes <bruno.guima00@gmail.com>
2025-06-03 18:11:24 -0300

    feat: ensure return 400 is returned when firstName is not provides

--- src/controllers/users/createUser/createUser.test.js
+++ src/controllers/users/createUser/createUser.test.js
@@ -30,6 +30,28 @@ describe('Create User Controller', () => {
         // Assert
 
         expect(result.statusCode).toBe(201);
-        expect(result.body).not.toBeUndefined();
+        expect(result.body).toEqual(httpRequest.body);
+    });
+
+    it('should return 400 if firstName is not provided', async () => {
+        // arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+
+        const createUserController = new CreateUserController(
+            createUserUseCase
+        );
+
+        const httpRequest = {
+            body: {
+                lastName: 'Soares',
+                email: 'bruno@bruno.com.br',
+                password: '1234456789',
+            },
+        };
+        // Act
+        const result = await createUserController.execute(httpRequest);
+
+        // Assert
+        expect(result.statusCode).toBe(400);
     });
 });
