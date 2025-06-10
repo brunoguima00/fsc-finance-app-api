9f4fdc25bd832fdecd52001da4547c1032b52573
Bruno Guimaraes <bruno.guima00@gmail.com>
2025-06-10 10:22:12 -0300

    refactor: refactor

--- src/controllers/helpers/validation.js
+++ src/controllers/helpers/validation.js
@@ -4,36 +4,11 @@ import { badRequest } from './httpHelper.js';
 export const checkIfIdIsValid = (id) => validator.isUUID(id);
 
 export const invalidIdResponse = () =>
-    badRequest({ message: 'The provider id is not valid' });
-
-export const checkIfIsString = (value) => typeof value === 'string';
-
-export const requiredFieldsIsMissingResponse = (field) => {
     badRequest({
-        message: `The field ${field} is required`,
+        message: 'The provided id is not valid.',
     });
-};
 
-export const validateRequiredFields = (params, requiredFields) => {
-    for (const field of requiredFields) {
-        const fieldIsMissing = !params[field];
-
-        const fieldIsEmpty =
-            checkIfIsString(params[field]) &&
-            validator.isEmpty(params[field], {
-                ignore_whitespace: true,
-            });
-
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            };
-        }
-    }
-
-    return {
-        ok: true,
-        missingField: undefined,
-    };
-};
+export const requiredFieldIsMissingResponse = (field) =>
+    badRequest({
+        message: `The field ${field} is required.`,
+    });
