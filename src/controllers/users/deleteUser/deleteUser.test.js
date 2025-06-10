da6220b21911b441a162ea44bda9fcd5eb3e1aa8
Bruno Guimaraes <bruno.guima00@gmail.com>
2025-06-09 21:03:40 -0300

    feat: ensure DeleteUserController returns 200 when user is deleted

---
+++ src/controllers/users/deleteUser/deleteUser.test.js
@@ -0,0 +1,42 @@
+import { DeleteUserController } from './deleteUserController';
+import { faker } from '@faker-js/faker';
+
+describe('DeleteUserControle', () => {
+    class DeleteUserUseCaseStub {
+        execute() {
+            return {
+                id: faker.string.uuid(),
+                firstName: faker.person.firstName(),
+                lastName: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 7,
+                }),
+            };
+        }
+    }
+
+    const makeSut = () => {
+        const deleteUserUseCase = new DeleteUserUseCaseStub();
+        const sut = new DeleteUserController(deleteUserUseCase);
+
+        return { deleteUserUseCase, sut };
+    };
+
+    const httpRequest = {
+        params: {
+            userId: faker.string.uuid(),
+        },
+    };
+
+    it('should return 200 if user is deleted', async () => {
+        //  Arrange
+        const { sut } = makeSut();
+
+        // act
+        const result = await sut.execute(httpRequest);
+
+        //assert
+        expect(result.statusCode).toBe(200);
+    });
+});
